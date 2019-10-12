212df14b65ef760a08d3d032d02eefcd9ae9982b
jamesandrewpohadi <jamesandrewpohadi@gmail.com>
2019-10-12 21:32:50 +0800

    update backend2

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var MongoClient = require("mongodb").MongoClient;
 // var url = "mongodb://35.161.233.181:27017/";
 var url = "mongodb://localhost:27017/";
 
-var con = mysql.createConnection({
+var ms = mysql.createConnection({
   // host: "18.210.39.176",
   host: "localhost",
   user: "root",
@@ -22,10 +22,10 @@ router.get("/", function(req, res, next) {
     dbo
       .collection("meta_Kindle_Store")
       .find({})
-      .limit(100)
+      .limit(20)
       .toArray(function(err, result) {
+        res.render("index", { data: { title: "goodreads", books: result } });
         console.log(result);
-        res.render("index", { title: "goodreads", books: result });
         db.close();
       });
   });
@@ -39,18 +39,14 @@ router.get("/book/:id", function(req, res, next) {
       .collection("meta_Kindle_Store")
       .find({ asin: req.params.id })
       .toArray(function(err, book) {
-        // console.log(book);
-        // con.connect(function(err) {
-        //   if (err) throw err;
-        //   console.log("Connected!");
-
-        // });
-        con.query(
+        ms.query(
           "select * from kindle_reviews where asin = '" + req.params.id + "'",
           function(err, reviews) {
             if (err) throw err;
             // console.log("reviews", reviews);
-            res.render("book_review", { book: book[0], reviews: reviews });
+            res.render("book_review", {
+              data: { book: book[0], reviews: reviews }
+            });
           }
         );
         db.close();

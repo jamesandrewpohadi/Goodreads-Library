3390779fde6c2158091f8bcf69a6410df570d174
edricwu <edricwu008@gmail.com>
2019-10-12 22:14:50 +0800

    add data for user page

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var mysql = require("mysql");
 var router = express.Router();
 
 var MongoClient = require("mongodb").MongoClient;
-// var url = "mongodb://35.163.65.254:27017/";
-var url = "mongodb://localhost:27017/";
+var url = "mongodb://35.163.65.254:27017/";
+// var url = "mongodb://localhost:27017/";
 
 var ms = mysql.createConnection({
-  // host: "18.210.39.176",
-  host: "localhost",
+  host: "18.210.39.176",
+  // host: "localhost",
   user: "root",
   password: "mysql",
   database: "goodreads"
@@ -52,6 +52,29 @@ router.get("/book/:id", function(req, res, next) {
         db.close();
       });
   });
+});
+
+router.get("/user/:name", function(req, res, next) {
+  ms.query(
+    "select * from kindle_reviews where reviewerName = '" + req.params.name + "'",
+    function(err, user_data){
+      if (err) throw err;
+      MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        dbo = db.db("goodreads");
+        dbo
+          .collection("meta_Kindle_Store")
+          .find({ asin: user_data[0].asin })
+          .toArray(function(err,book_data){
+            res.render("user", {
+              data: {user_data: user_data, book_data: book_data}
+            });
+          });
+          db.close();
+      });
+    }
+  )
+  
 });
 
 module.exports = router;

dc9759d4ae9ec0ef56f00882ff89b84df9a7d467
jamesandrewpohadi <jamesandrewpohadi@gmail.com>
2019-10-25 23:51:07 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ var router = express.Router();
 var book_id = [];
 
 var MongoClient = require("mongodb").MongoClient;
-var url = "mongodb://35.163.65.254:27017/";
-// var url = "mongodb://localhost:27017/";
+// var url = "mongodb://35.163.65.254:27017/";
+var url = "mongodb://localhost:27017/";
 
 var ms = mysql.createConnection({
-  host: "18.210.39.176",
-  // host: "localhost",
+  // host: "18.210.39.176",
+  host: "localhost",
   user: "root",
   password: "mysql",
   database: "goodreads"
@@ -46,7 +46,10 @@ router.get("/", function(req, res, next) {
       .find({})
       .limit(20)
       .toArray(function(err, result) {
-        
+        console.log(result);
+        // result.forEach(books => {
+
+        // });
         res.render("index", { data: { title: "goodreads", books: result } });
         db.close();
       });
@@ -65,8 +68,6 @@ router.get("/book/:id", function(req, res, next) {
           "select * from kindle_reviews where asin = '" + req.params.id + "'",
           function(err, reviews) {
             if (err) throw err;
-            // console.log("reviews", reviews);
-            // console.log("book", book[0])
             res.render("book_review", {
               data: { book: book[0], reviews: reviews }
             });
@@ -80,51 +81,46 @@ router.get("/book/:id", function(req, res, next) {
 router.get("/user/:id", function(req, res, next) {
   ms.query(
     "select * from kindle_reviews where reviewerID = '" + req.params.id + "'",
-    function(err, user_data){
-      
-      var keyCount  = Object.keys(user_data).length;
-      
-      for (var i = 0; i<keyCount; i++){
-        book_id.push(user_data[i]['asin'])
+    function(err, user_data) {
+      var keyCount = Object.keys(user_data).length;
+
+      for (var i = 0; i < keyCount; i++) {
+        book_id.push(user_data[i]["asin"]);
       }
-      console.log(keyCount)
-      console.log(book_id)
+      console.log(keyCount);
+      console.log(book_id);
       if (err) throw err;
       MongoClient.connect(url, function(err, db) {
-      
         if (err) throw err;
         dbo = db.db("goodreads");
         dbo
           .collection("meta_Kindle_Store")
-          .find({asin: { $in: book_id }})
+          .find({ asin: { $in: book_id } })
           .limit(20)
-          .toArray(function(err,book_data){
-            
-            
+          .toArray(function(err, book_data) {
             res.render("user", {
-              
-              data: {user_data: user_data, book_data: book_data,title: "goodreads"}
-              
+              data: {
+                user_data: user_data,
+                book_data: book_data,
+                title: "goodreads"
+              }
             });
             console.log(user_data);
           });
-          
-          db.close();
+
+        db.close();
       });
     }
   );
 });
 
-router.get("/loginpage", function(req,res,next){
-
+router.get("/loginpage", function(req, res, next) {
   res.render("login");
-
 });
 
-router.post("/logindetails", function(req,res,next){
-
-  uname = req.body["uname"]
-  console.log(uname)
+router.post("/logindetails", function(req, res, next) {
+  uname = req.body["uname"];
+  console.log(uname);
 
   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
@@ -140,8 +136,6 @@ router.post("/logindetails", function(req,res,next){
       });
   });
   console.log("Yooooooo");
- 
-
 });
 
 module.exports = router;

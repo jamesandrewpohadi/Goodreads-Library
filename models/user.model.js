7a08a6bb344006cbea566eeb07e4a745fc2773f1
edricwu <edricwu008@gmail.com>
2019-11-19 18:49:02 +0800

    add login middlewares

--- models/user.model.js
+++ models/user.model.js
@@ -1,13 +1,15 @@
 const config = require('config');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const mysql = require('mysql');
 const Joi = require('joi');
 
 var ms = mysql.createConnection({
-    host: "18.210.39.176",
-    //host: "localhost",
-    user: "root",
-    password: "mysql",
-    database: "goodreads"
+  host: "52.220.10.107",
+  //host: "localhost",
+  user: "root",
+  password: "mysql",
+  database: "goodreads"
 });
 
 //function to validate user 
@@ -21,7 +23,66 @@ function validateUser(user) {
   return Joi.validate(user, schema);
 }
 
-console.log(validateUser({name: 'aaaa', email: 'aaaaaa', password:'bbbb'}));
-console.log('a');
-// exports.User = User; 
+function registerUser(email, name, password){
+  const {error} = validateUser({ name: name, email: email, password: password })
+  if (!error){
+    bcrypt.hash('aa',10, function(err,hash){
+      ms.query(
+        "INSERT INTO user_data VALUES (?,?,?)",
+        [email, name, hash],
+        function (err, aaa) {
+          // console.log(aaa);
+          if (err){
+            console.log(err.sqlMessage);
+          }
+          else{
+            console.log("Successfully register new user!");
+          }
+          ms.destroy();
+        }
+      );
+    });
+  }
+  else{
+    console.log(error.details[0].message);
+  }
+}
+
+function login(email, password){
+  ms.query(
+    "SELECT password FROM user_data WHERE email = ?",
+    [email],
+    function (err, res){
+      if (err){
+        console.log(1);
+      }
+      else {
+        if (!res){
+          console.log("User not found!");
+        }
+        else{
+          bcrypt.compare(password, res[0].password,
+            function(err,res){
+              if (res){
+                console.log("Successfully login!");
+              }
+              else {
+                console.log("Incorrect password!");
+              }
+            })
+          ms.destroy();
+        }
+      }
+    }
+  )
+} 
+
+function generateToken(email){
+  const token = jwt.sign({email: email}, config.get("myprivatekey"));
+  return token;
+}
+
+exports.login = login;
+exports.register = registerUser;
 exports.validate = validateUser;
+exports.generate = generateToken;
--- test.js
+++ test.js
@@ -1,39 +1,89 @@
-var mysql = require("mysql");
-var MongoClient = require("mongodb").MongoClient;
-var url = "mongodb://localhost:27017/";
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const mysql = require('mysql');
+const Joi = require('joi');
 
-var con = mysql.createConnection({
-  host: "localhost",
+var ms = mysql.createConnection({
+  host: "52.220.10.107",
+  //host: "localhost",
   user: "root",
   password: "mysql",
   database: "goodreads"
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  con.query("select * from kindle_reviews where asin = 'B000FA64QO'", function(
-    err,
-    result
-  ) {
-    if (err) throw err;
-    console.log(result[0]);
-  });
-});
+//function to validate user 
+function validateUser(user) {
+  const schema = {
+    name: Joi.string().min(3).max(50).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(3).max(255).required()
+  };
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  dbo = db.db("goodreads");
-  dbo
-    .collection("meta_Kindle_Store")
-    .find({})
-    .limit(3)
-    .toArray(function(err, result) {
-      // if (err) {
-      //   console.log("errr");
-      //   throw err;
-      // };
-      // console.log(result);
-      db.close();
+  return Joi.validate(user, schema);
+}
+
+function registerUser(email, name, password){
+  const {error} = validateUser({ name: name, email: email, password: password })
+  if (!error){
+    bcrypt.hash('aa',10, function(err,hash){
+      ms.query(
+        "INSERT INTO user_data VALUES (?,?,?)",
+        [email, name, hash],
+        function (err, aaa) {
+          // console.log(aaa);
+          if (err){
+            console.log(err.sqlMessage);
+          }
+          else{
+            console.log("Successfully register new user!");
+          }
+          ms.destroy();
+        }
+      );
     });
-});
+  }
+  else{
+    console.log(error.details[0].message);
+  }
+}
+
+function login(email, password){
+  ms.query(
+    "SELECT password FROM user_data WHERE email = ?",
+    [email],
+    function (err, res){
+      if (err){
+        console.log(err);
+      }
+      else {
+        if (!res){
+          console.log("User not found!");
+        }
+        else{
+          bcrypt.compare(password, res[0].password,
+            function(err,res){
+              if (res){
+                console.log("Successfully login!");
+              }
+              else {
+                console.log("Incorrect password!");
+              }
+            })
+          ms.destroy();
+        }
+      }
+    }
+  )
+}
+
+login("aaa","aaa");
+// console.log(validateUser({ name: 'aaaa', email: 'aaaaa', password: 'bbbb' }));
+// login('aa','ab');
+// console.log(crypto.randomBytes(128).toString('hex'));
+
+// const { error } = validateUser({ name: 'aaaa', email: 'aaaaaa@aa.aa', password: 'bbbb' });
+// console.log(error);
+// console.log('a');
+// exports.User = User; 
+// exports.validate = validateUser;

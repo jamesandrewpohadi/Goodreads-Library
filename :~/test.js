19f9874711c1f9c23ae1fa12aa6db569bf93df8e
Kundan Dalmia <kundandalmia@gmail.com>
2019-12-03 19:41:20 +0800

    updated mysql instance and delete unnecessary folders
    updated mysql instance and delete unnecessary folders

--- :~/test.js
+++
@@ -1,5 +0,0 @@
-// var datetime = new Date().toLocaleDateString();
-// console.log(datetime.slice(0,2) + " " + datetime.slice(3,5) + ", " + datetime.slice(6,10));
-
-var datetime = new Date().valueOf();
-console.log((datetime/1000) >> 0);

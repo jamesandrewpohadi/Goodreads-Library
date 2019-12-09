2e5c8f771c6655aba47209c2b3b9de8008a2973e
RealTanYou <tan.y@outlook.sg>
2019-12-09 04:19:59 -0800

    updated hadoop files for spark

--- hadoop/conf_node_master.sh
+++ hadoop/conf_node_master.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 cd /home/ubuntu
-mv hadoop-3.1.2 hadoop
-chown -R ubuntu: hadoop
+apt-get -y install jq
+source /home/ubuntu/.profile
+source /home/ubuntu/.bashrc
 node_m_addr=$(cat analytics_instances.json | jq '.node_master.publicdns')
+node_priv=$(cat analytics_instances.json | jq '.node_master.privateip')
 node_m_addr=$(echo "${node_m_addr//\"}")
 worker_addr=($(cat analytics_instances.json | jq 'to_entries[] | select(.key|test("worker_node.")) | .value.publicdns'))
 worker_priv=($(cat analytics_instances.json | jq 'to_entries[] | select(.key|test("worker_node.")) | .value.privateip'))
-sed -i "s/# export JAVA_HOME=/export JAVA_HOME=\/usr\/lib\/jvm\/java-8-openjdk-amd64\/jre/g" /home/ubuntu/hadoop/etc/hadoop/hadoop-env.sh
-sed -i "/<\/configuration>/i <property>\n<name>fs.defaultFS<\/name>\n<value>hdfs:\/\/${node_m_addr}:9000<\/value>\n<\/property>\n" /home/ubuntu/hadoop/etc/hadoop/core-site.xml
-sed -i "/<\/configuration>/i <property>\n<name>dfs.namenode.name.dir<\/name>\n<value>\/home\/ubuntu\/name<\/value>\n<\/property>\n<property>\n<name>dfs.datanode.data.dir<\/name>\n<value>\/home\/ubuntu\/data<\/value>\n<\/property>\n<property>\n<name>dfs.replication<\/name>\n<value>1<\/value>\n<\/property>\n" /home/ubuntu/hadoop/etc/hadoop/hdfs-site.xml
-sed -i '/<\/configuration>/i <property>\n<name>mapreduce.framework.name<\/name>\n<value>yarn<\/value>\n<\/property>\n<property>\n<name>yarn.app.mapreduce.am.env<\/name>\n<value>HADOOP_MAPRED_HOME=$HADOOP_HOME<\/value>\n<\/property>\n<property>\n<name>mapreduce.map.env<\/name>\n<value>HADOOP_MAPRED_HOME=$HADOOP_HOME<\/value>\n<\/property>\n<property>\n<name>mapreduce.reduce.env<\/name>\n<value>HADOOP_MAPRED_HOME=$HADOOP_HOME<\/value>\n<\/property>\n<property>\n<name>yarn.app.mapreduce.am.resource.mb<\/name>\n<value>512<\/value>\n<\/property>\n<property>\n<name>mapreduce.map.memory.mb<\/name>\n<value>512<\/value>\n<\/property>\n<property>\n<name>mapreduce.reduce.memory.mb<\/name>\n<value>256<\/value>\n<\/property>\n' /home/ubuntu/hadoop/etc/hadoop/mapred-site.xml
-sed -i "/<\/configuration>/i <property>\n<name>yarn.acl.enable<\/name>\n<value>0<\/value>\n<\/property>\n<property>\n<name>yarn.resourcemanager.hostname<\/name>\n<value>${node_m_addr}<\/value>\n<\/property>\n<property>\n<name>yarn.nodemanager.aux-services<\/name>\n<value>mapreduce_shuffle<\/value>\n<\/property>\n<property>\n<name>yarn.nodemanager.resource.memory-mb<\/name>\n<value>640<\/value>\n<\/property>\n<property>\n<name>yarn.scheduler.maximum-allocation-mb<\/name>\n<value>640<\/value>\n<\/property>\n<property>\n<name>yarn.scheduler.minimum-allocation-mb<\/name>\n<value>128</value>\n<\/property>\n<property>\n<name>yarn.nodemanager.vmem-check-enabled<\/name>\n<value>false<\/value>\n<\/property>\n" /home/ubuntu/hadoop/etc/hadoop/yarn-site.xml
-rm /home/ubuntu/hadoop/etc/hadoop/workers
-touch /home/ubuntu/hadoop/etc/hadoop/workers
+
+cp /home/ubuntu/spark/conf/slaves.template /home/ubuntu/spark/conf/slaves
+echo "" >> /home/ubuntu/spark/conf/slaves
 for worker in ${worker_addr[@]}
 do
 worker=$(echo "${worker//\"}")
-echo $worker >> /home/ubuntu/hadoop/etc/hadoop/workers
+echo $worker >> /home/ubuntu/spark/conf/slaves
 done
-for worker in ${worker_priv[@]}
-do
-worker=$(echo "${worker//\"}")
-scp -o "StrictHostKeyChecking=no" -i /home/ubuntu/.ssh/id_rsa /home/ubuntu/hadoop/etc/hadoop/* "ubuntu@${worker}:/home/ubuntu/hadoop/etc/hadoop/"
-done
-runuser -l ubuntu -c 'hdfs namenode -format'
+runuser -l ubuntu -c './spark/sbin/start-master.sh'
+runuser -l ubuntu -c './spark/sbin/start-slaves.sh'
+#runuser -l ubuntu -c "spark-submit --master spark://${node_m_addr}:7077 "
--- hadoop/hadoop_node.sh
+++ hadoop/hadoop_node.sh
@@ -4,12 +4,19 @@ runuser -l ubuntu -c 'ssh-keygen -b 4096 -f "/home/ubuntu/.ssh/id_rsa" -N ""'
 cat /home/ubuntu/.ssh/id_rsa.pub >> /home/ubuntu/.ssh/authorized_keys
 apt-get update
 apt-get -y upgrade
+apt-get -y install python-software-properties
+apt-get -y install python3-pip
+pip3 --no-cache-dir install pyspark
 apt-get -y install jq
 apt-get -y install openjdk-8-jdk
-wget https://archive.apache.org/dist/hadoop/core/hadoop-3.1.2/hadoop-3.1.2.tar.gz
-tar -xzf hadoop-3.1.2.tar.gz
-chown -R ubuntu: hadoop-3.1.2
-mv hadoop-3.1.2 hadoop
-echo 'PATH=/home/ubuntu/hadoop/bin:/home/ubuntu/hadoop/sbin:$PATH' >> /home/ubuntu/.profile
-echo 'export HADOOP_HOME=/home/ubuntu/hadoop' >> /home/ubuntu/.bashrc
-echo 'export PATH=${PATH}:${HADOOP_HOME}/bin:${HADOOP_HOME}/sbin' >> /home/ubuntu/.bashrc
+wget https://archive.apache.org/dist/spark/spark-2.4.4/spark-2.4.4-bin-hadoop2.7.tgz
+tar -xvf spark-2.4.4-bin-hadoop2.7.tgz
+chown -R ubuntu: spark-2.4.4-bin-hadoop2.7
+mv spark-2.4.4-bin-hadoop2.7 spark
+cp /home/ubuntu/spark/conf/spark-env.sh.template /home/ubuntu/spark/conf/spark-env.sh
+echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> /home/ubuntu/.bashrc
+echo 'export JRE_HOME=$JAVA_HOME/jre' >> /home/ubuntu/.bashrc
+echo 'export PATH=$PATH:$JAVA_HOME/bin:$JAVA_HOME/jre/bin' >> /home/ubuntu/.bashrc
+echo 'export SPARK_HOME=/home/ubuntu/spark' >> /home/ubuntu/.bashrc
+echo 'export PYSPARK_PYTHON=/usr/bin/python3' >> /home/ubuntu/spark/conf/spark-env.sh
+source ~/.bashrc

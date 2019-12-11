9b97cb684d9a2d281cf2f1f502bd97f11c03970c
RealTanYou <tan.y@outlook.sg>
2019-12-11 00:51:59 -0800

    update for hadoop

--- ETL.sh
+++ ETL.sh
@@ -3,7 +3,7 @@
 # when running analytics should scp instance json to hdfs master
 mysql=$(grep  -oP '"mysql-server": \"\K(.*?)"' web/instance.json | tr -d \")
 mongodb=$(grep  -oP '"mongodb-server": \"\K(.*?)"' web/instance.json | tr -d \")
-web=$(grep  -oP '"web-server": \"\K(.*?)"' web/instance.json | tr -d \")
+#web=$(grep  -oP '"web-server": \"\K(.*?)"' web/instance.json | tr -d \")
 
 sudo apt update
 sudo apt install -y mongodb
@@ -15,5 +15,5 @@ sudo apt install mysql-server -y
 # sudo mysql -e "ALTER USER '%'@'%' IDENTIFIED WITH mysql_native_password BY 'mysql';"
 # sudo mysql -e "GRANT ALL ON goodreads.kindle_reviews TO '%'@'%';"
 
-sudo mysql --password=mysql goodreads -h mysql -e 'select * from kindle_reviews;' > kindle_reviews.tsv
+sudo mysql --password=mysql goodreads -h $mysql -e 'select * from kindle_reviews;' > kindle_reviews.tsv
 mongoexport -h $mongodb --collection=meta_Kindle_Store --db=goodreads --out=meta_kindle.json
--- create_web_instances.py
+++ create_web_instances.py
@@ -7,7 +7,7 @@ ec2_resource = boto3.resource('ec2')
 def credel_web_instances():
     response = ec2_client.describe_vpcs()
     vpc_id = response.get('Vpcs', [{}])[0].get('VpcId', '')
-    sgs = ec2_client.create_security_group(GroupName='SECURITY_GROUP3',Description='DESCRIPTION',VpcId=vpc_id)
+    sgs = ec2_client.create_security_group(GroupName='SECURITY_GROUP2',Description='DESCRIPTION',VpcId=vpc_id)
     security_group_id = sgs['GroupId']
     data = ec2_client.authorize_security_group_ingress(
         GroupId=security_group_id,
@@ -30,9 +30,9 @@ def credel_web_instances():
             'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
             ]
             )
-    mongodb = ec2_resource.create_instances(ImageId='ami-0a092d402bc88d017', InstanceType='t2.micro',MinCount=1,MaxCount=1,KeyName='testKey',SecurityGroupIds=[security_group_id])
-    mysql = ec2_resource.create_instances(ImageId='ami-083fb6a9e21c34688', InstanceType='t2.micro',MinCount=1,MaxCount=1,KeyName='testKey',SecurityGroupIds=[security_group_id])
-    web = ec2_resource.create_instances(ImageId='ami-06861bded36d551dd', InstanceType='t2.micro',MinCount=1,MaxCount=1,KeyName='testKey',SecurityGroupIds=[security_group_id])
+    mongodb = ec2_resource.create_instances(ImageId='ami-0a092d402bc88d017', InstanceType='t2.micro',MinCount=1,MaxCount=1,KeyName='sing_net_test',SecurityGroupIds=[security_group_id])
+    mysql = ec2_resource.create_instances(ImageId='ami-083fb6a9e21c34688', InstanceType='t2.micro',MinCount=1,MaxCount=1,KeyName='sing_net_test',SecurityGroupIds=[security_group_id])
+    web = ec2_resource.create_instances(ImageId='ami-06861bded36d551dd', InstanceType='t2.micro',MinCount=1,MaxCount=1,KeyName='sing_net_test',SecurityGroupIds=[security_group_id])
     # instancesIds = [inst.id for inst in instances]
     mongodb[0].wait_until_running()
     mysql[0].wait_until_running()
